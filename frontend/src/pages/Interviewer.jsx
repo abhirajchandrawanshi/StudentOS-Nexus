3eaa9b1d3a4188a6c0d8614c1349b114dd7d5bae
Sakshi Chandravanshi <sakshichandravanshi15@gmail.com>
2026-05-19 13:19:23 +0530

    Delete dummy frontend directory

--- frontend/src/pages/Interviewer.jsx
+++
@@ -1,9 +0,0 @@
-const Interviewer = () => {
-  return (
-    <div className="text-white text-3xl p-10">
-      Interviewer Page
-    </div>
-  );
-};
-
-export default Interviewer;
--- frontend/src/pages/Recommendations.jsx
+++
@@ -1,9 +0,0 @@
-const Recommendations = () => {
-  return (
-    <div className="text-white text-3xl p-10">
-      Recommendations Page
-    </div>
-  );
-};
-
-export default Recommendations;
